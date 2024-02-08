dbf53bb0fe654f72cd37c9823e0ee1fa604ae8a4
kriubi <41031492+kriubi@users.noreply.github.com>
2024-02-07 21:44:01 -0300

    project start

---
+++ src/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT;
+const CONSUMER_KEY = process.env.CONSUMER_KEY;
+const CONSUMER_SECRET = process.env.CONSUMER_SECRET;
+const CODE = process.env.CODE;
+
+app.set('view engine', 'pug');
+app.set('views', '/usr/src/app/src/views');
+
+app.get('/', (req, res) => {
+    return res.sendStatus(200);
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+module.exports = app;
