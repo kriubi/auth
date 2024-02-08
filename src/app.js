60b94eda347cc287a4261b386359191d30ecdb86
kriubi <41031492+kriubi@users.noreply.github.com>
2024-02-08 18:31:24 -0300

    testing

--- src/app.js
+++ src/app.js
@@ -4,16 +4,30 @@ const PORT = process.env.PORT;
 const CONSUMER_KEY = process.env.CONSUMER_KEY;
 const CONSUMER_SECRET = process.env.CONSUMER_SECRET;
 const CODE = process.env.CODE;
+const VERCEL_URL = process.env.VERCEL_URL;
+
+let access_token = null;
+let refresh_token = null;
+let globalCode = null;
 
 app.set('view engine', 'pug');
 app.set('views', '/usr/src/app/src/views');
 
 app.get('/', (req, res) => {
-    return res.sendStatus(200);
-});
 
-app.get('/test', (req, res) => {
-    return res.status(200).send(process.env.VERCEL_URL);
+    const { url, adm_user, store } = req.query;
+    
+    const { code, api_address, store_host } = req.query;
+    
+    return res.status(200).send({
+        url,
+        adm_user,
+        store,
+        code,
+        api_address,
+        store_host
+    });
+
 });
 
 app.listen(PORT, () => {
