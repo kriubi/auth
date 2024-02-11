b3ce2a45b5aabd9e55aaa19810a394ca2658d218
kriubi <41031492+kriubi@users.noreply.github.com>
2024-02-11 14:10:12 -0300

    testing

--- src/app.js
+++ src/app.js
@@ -13,16 +13,21 @@ app.set('view engine', 'pug');
 app.set('views', __dirname + '/views');
 
 app.get('/', async (req, res) => {
+
     const title = "/";
+
     const _now = new Date().toLocaleString('pt-BR', { timeZone: "America/Sao_Paulo" });
-    let logInformation = null;
+
+    let logInformation = "not authorized yet";
+
     const { url, adm_user, store } = req.query;
+
     const { code, api_address, store_host } = req.query;
+
     let auth_callback = "http://" + VERCEL_URL + "/callback";
 
     if (globalCode != null) {
         let message = `${_now} - path: / - url: ${url} - adm_user: ${adm_user} - store: ${store} - code: ${code} - api_address: ${api_address} - store_host: ${store_host}`;
-        console.log(message);
         logInformation = await log(message);
     }
 
@@ -42,16 +47,21 @@ app.get('/', async (req, res) => {
 });
 
 app.get('/callback', async (req, res) => {
+
     const title = "/callback";
+
     const _now = new Date().toLocaleString('pt-BR', { timeZone: "America/Sao_Paulo" });
-    let logInformation = null;
+
+    let logInformation = "not authorized yet";
+
     const { url, adm_user, store } = req.query;
+
     const { code, api_address, store_host } = req.query;
+
     let auth_callback = "http://" + VERCEL_URL + "/callback";
 
     if (globalCode != null) {
         let message = `${_now} - path: /callback - url: ${url} - adm_user: ${adm_user} - store: ${store} - code: ${code} - api_address: ${api_address} - store_host: ${store_host}`;
-        console.log(message);
         logInformation = await log(message);
     }
 
@@ -70,16 +80,14 @@ app.get('/callback', async (req, res) => {
 });
 
 app.listen(PORT, () => {
-    logger.info(`server is running on port ${PORT}`);
+    console.log(`server is running on port ${PORT}`);
 });
 
 module.exports = app;
 
 async function log(message) {
-
     let tokensGot = await GetTokens();
-
-    if (!tokensGot) {
+    if (tokensGot == null) {
         return "unable to get tokens";
     }
 
@@ -116,25 +124,18 @@ async function log(message) {
 async function GetTokens() {
     try {
         const authEndpoint = "http://1039992.commercesuite.com.br/web_api/auth";
-
         const response = await fetch(authEndpoint, {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({
                 consumer_key: CONSUMER_KEY,
                 consumer_secret: CONSUMER_SECRET,
-                code: CODE,
+                code: globalCode,
             }),
         });
-
         const result = await response.json();
-
-        access_token = result.access_token;
-
-        refresh_token = result.refresh_token;
-
-        return true;
+        return result.access_token;
     } catch (error) {
-        return false;
+        return null;
     }
 }
